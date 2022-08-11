7485fc310a4218a51830ab0913b9990dee37857a
Ulyanna <ulyanna16@gmail.com>
2022-08-11 17:40:14 +0300

    done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const divBoxParentEl = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const galleryMarkup = createGallaryMarkup(galleryItems);
+divBoxParentEl.insertAdjacentHTML('beforeend', galleryMarkup);
+
+divBoxParentEl.addEventListener('click', handleImageClick);
+
+function createGallaryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+     <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a> `;
+    })
+    .join('');
+}
+
+function createModalWindow() {
+  return basicLightbox.create(
+    `
+        <img src="${event.target.dataset.source}">
+    `
+  );
+  // .show();
+}
+
+function handleImageClick(event) {
+  event.preventDefault();
+
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  }
+  createModalWindow().show();
+  console.log(basicLightbox.visible());
+}
+
+// //////////////////////////////////////////////////////////
+
+const bodyEl = document.querySelector('body');
